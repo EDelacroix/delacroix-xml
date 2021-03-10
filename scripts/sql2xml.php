9c3f99feedf534b6aeca86adbc24205eecec4440
Frédéric Glorieux <frederic.glorieux@fictif.org>
2021-03-10 16:59:41 +0100

    Doc, allez, on code !

---
+++ scripts/sql2xml.php
@@ -0,0 +1,11 @@
+<?php
+$server = "localhost";
+$user = "root";
+$pass = "PiedNez";
+$base = "delacroix";
+
+$pdo = new PDO("mysql:host=$server;dbname=$base", $user, $pass);
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+echo "Connecté"; 
+
+?>
