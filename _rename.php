dd69c7b685e2881b501eba44171a95656e4fa7c0
glorieux-f <fred@CL-PF0VWNT9.isis.unige.ch>
2022-07-07 02:37:55 +0200

    The bad Win bug

--- _rename.php
+++ _rename.php
@@ -1,13 +1,13 @@
 <?php
 $dir = __DIR__;
-foreach (scandir(__DIR__) as $src_file) {
-    echo $src_file."\n";
-    continue;
+foreach (glob(__DIR__.'/*.xml') as $src_file) {
+    // echo $src_file."\n";
     $src_dir = dirname($src_file);
     $src_name = basename($src_file);
     $dst_name = "CAC_ED_" . mb_strtolower(substr($src_name, 7));
-    // if ($src_name == $dst_name) continue;
+    if ($src_name == $dst_name) continue;
     echo "Delete" . $src_name . "\n";
+    unlink($src_file);
     // rename($src_file, $src_dir . '/' . $dst_name);
 }
 
